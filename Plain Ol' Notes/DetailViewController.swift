2f1f26c72bfe03857f89665aafe43784f8af9d77
tonidemingo <tonidemingo@gmail.com>
2015-04-19 23:52:11 +0200

    Added link to textView

--- Plain Ol' Notes/DetailViewController.swift
+++ Plain Ol' Notes/DetailViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var tView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
